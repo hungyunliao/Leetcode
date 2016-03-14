60603410537c3fec815a972ca44990c0a145d9fb
Hung-Yun Liao <hungyunliao@gmail.com>
2016-03-14 11:36:29 -0700

    Update Set Matrix Zeroes.java

--- Set Matrix Zeroes.java
+++ Set Matrix Zeroes.java
@@ -8,23 +8,15 @@ public class Solution {
         for(int i = 0; i < height; i++) {
             for(int j = 0; j < width; j++) {
                 if(matrix[i][j] == 0) {
-                    if(!row.contains(i)) {
-                        row.add(i);
-                    }
-                    if(!col.contains(j)) {
-                        col.add(j);
-                    }
+                    if(!row.contains(i)) { row.add(i); }
+                    if(!col.contains(j)) { col.add(j); }
                 }
             }
         }
         for(int i = 0; i < height; i++) {
             for(int j = 0; j < width; j++) {
-                if(row.contains(i)) {
-                    matrix[i][j] = 0;
-                }
-                if(col.contains(j)) {
-                    matrix[i][j] = 0;
-                }
+                if(row.contains(i)) { matrix[i][j] = 0; }
+                else if(col.contains(j)) { matrix[i][j] = 0; }
             }
         }
     }
