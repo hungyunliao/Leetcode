472a4384a0936adb3143591ebd605ee3b2dc8d38
Hung-Yun Liao <hungyunliao@gmail.com>
2016-03-17 03:27:03 -0700

    Update Unique Paths(recursive version).java

--- Unique Paths(recursive version).java
+++ Unique Paths(recursive version).java
@@ -16,3 +16,47 @@ public class Solution {
         maze(m, n - 1);
     }
 }
+
+
+public class Solution {
+    int count = 0;
+    
+    public int uniquePaths(int m, int n) {
+        maze(m, n);
+        return count;
+    }
+    
+    private void maze(int m, int n) {
+        if(m == 4) {
+            for(int i = n; i > 0; i--) {
+                count += cal(i);
+            }
+            return;
+        }
+        if(n == 4) {
+            for(int i = m; i > 0; i--) {
+                count += cal(i);
+            }
+            return;
+        }
+        // if(m == 3) {
+        //     count += cal(n);
+        //     return;
+        // }
+        // if(n == 3) {
+        //     count += cal(m);
+        //     return;
+        // }
+        
+        if(m < 1 || n < 1) { return; }
+        maze(m - 1, n);
+        maze(m, n - 1);
+    }
+    private int cal(int k) {
+        int sum = 0;
+        for(int i = 1; i <= k; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
