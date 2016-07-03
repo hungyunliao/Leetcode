fe8baba4d67c73f970401dcded2b0dbb6d330927
Hung-Yun Liao <hungyunliao@gmail.com>
2016-07-03 16:24:53 -0700

    Update Reverse String.java

--- Reverse String.java
+++ Reverse String.java
@@ -1,9 +1,10 @@
 public class Solution {
     public String reverseString(String s) {
         StringBuilder sb = new StringBuilder();
-        int leng = s.length();
-        for (int i = leng - 1; i >= 0; --i) {
-            sb.append(s.charAt(i));
+        // Faster than using s.charAt(i)
+        char[] sArray = s.toCharArray();
+        for (int i = sArray.length - 1; i >= 0; --i) {
+            sb.append(sArray[i]);
         }
         return sb.toString();
     }
