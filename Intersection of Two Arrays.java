d9c7d2a3750fb02055cc89a2a0f0829fd0aa7622
Hung-Yun Liao <hungyunliao@gmail.com>
2016-07-04 13:49:33 -0700

    Update Intersection of Two Arrays.java

--- Intersection of Two Arrays.java
+++ Intersection of Two Arrays.java
@@ -1,23 +1,19 @@
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int length = Math.min(nums1.length, nums2.length);
-        int[] ans = new int[length];
+        if (nums1 == null || nums2 == null) { return null; }
+        int[] ans = new int[nums1.length];
         int index = 0;
         HashSet<Integer> numberInTheShortArray = new HashSet<Integer>();
         
-        if (nums1.length <= nums2.length) {
-            for (int i = 0; i < nums1.length; ++i) { numberInTheShortArray.add(nums1[i]); }
-            index = saveTheIdenticalNumberInHashSet(numberInTheShortArray, nums2, ans);
-            
-        } else {
-            for (int i = 0; i < nums2.length; ++i) { numberInTheShortArray.add(nums2[i]); }
-            index = saveTheIdenticalNumberInHashSet(numberInTheShortArray, nums1, ans);
-        }
+        // Algo:
+        // 1. remove the duplicate elements in the nums1 (hs)
+        // 2. binary search each elements in the hs in within nums2. If returning true, add it into the ans array
+        // 3. trim the ans array by Arrays.copyOf Range
+        for (int i = 0; i < nums1.length; ++i) { numberInTheShortArray.add(nums1[i]); }
+        index = saveTheIdenticalNumberInHashSet(numberInTheShortArray, nums2, ans);
         
-        int[] retVal = new int[index];
-        for (int i = 0; i < retVal.length; ++i) { retVal[i] = ans[i]; }
-        
-        return retVal;
+        // trim the array to avoid showing the value of '0's
+        return Arrays.copyOfRange(ans, 0, index); // return a new array with the length of index - 0
     }
     
     public int saveTheIdenticalNumberInHashSet(HashSet<Integer> hs, int[] array, int[] ans) {
