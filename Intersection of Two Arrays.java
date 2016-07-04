7a2ea480f59db2c8208aae99f737fb1523619403
Hung-Yun Liao <hungyunliao@gmail.com>
2016-07-04 12:30:14 -0700

    Update Intersection of Two Arrays.java

--- Intersection of Two Arrays.java
+++ Intersection of Two Arrays.java
@@ -6,36 +6,30 @@ public class Solution {
         HashSet<Integer> numberInTheShortArray = new HashSet<Integer>();
         
         if (nums1.length <= nums2.length) {
-            for (int i = 0; i < nums1.length; ++i) {
-                numberInTheShortArray.add(nums1[i]);
-            }
-            Arrays.sort(nums2);
-            for (Integer number : numberInTheShortArray) {
-                if (binarySearch(nums2, number)) {
-                    ans[index] = number;
-                    index++;
-                }
-            }
+            for (int i = 0; i < nums1.length; ++i) { numberInTheShortArray.add(nums1[i]); }
+            index = saveTheIdenticalNumberInHashSet(numberInTheShortArray, nums2, ans);
             
         } else {
-            for (int i = 0; i < nums2.length; ++i) {
-                numberInTheShortArray.add(nums2[i]);
-            }
-            System.out.println(numberInTheShortArray);
-            Arrays.sort(nums1);
-            for (Integer number : numberInTheShortArray) {
-                if (binarySearch(nums1, number)) {
-                    ans[index] = number;
-                    index++;
-                }
-            }
-        }
-        int[] retVal = new int[index];
-        for (int i = 0; i < retVal.length; ++i) {
-            retVal[i] = ans[i];
+            for (int i = 0; i < nums2.length; ++i) { numberInTheShortArray.add(nums2[i]); }
+            index = saveTheIdenticalNumberInHashSet(numberInTheShortArray, nums1, ans);
         }
         
+        int[] retVal = new int[index];
+        for (int i = 0; i < retVal.length; ++i) { retVal[i] = ans[i]; }
+        
         return retVal;
+    }
+    
+    public int saveTheIdenticalNumberInHashSet(HashSet<Integer> hs, int[] array, int[] ans) {
+        int index = 0;
+        Arrays.sort(array);
+        for (Integer number : hs) {
+            if (binarySearch(array, number)) {
+                ans[index] = number;
+                index++;
+            }
+        }
+        return index;
     }
     
     public boolean binarySearch(int[] array, int target) {
