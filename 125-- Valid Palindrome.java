21dfdc8c41b85369c4c065eae1ba44955ce450cb
Hung-Yun Liao <hungyunliao@gmail.com>
2016-08-05 13:50:58 -0700

    Update 125-- Valid Palindrome.java

--- 125-- Valid Palindrome.java
+++ 125-- Valid Palindrome.java
@@ -4,13 +4,13 @@ public class Solution {
         String str = s.toLowerCase();
         
         for (int i = 0, j = str.length() - 1; i < j;) {
-            while ((str.charAt(i) < 97 || str.charAt(i) >  122) && (str.charAt(i) < 48 || str.charAt(i) >  57)) {
+            while ((str.charAt(i) < 'a' || str.charAt(i) >  'z') && (str.charAt(i) < '0' || str.charAt(i) >  '9')) {
                 i++;
                 if (i > j) {
                     return true;
                 }
             }
-            while ((str.charAt(j) < 97 || str.charAt(j) >  122) && (str.charAt(j) < 48 || str.charAt(j) >  57)) {
+            while ((str.charAt(j) < 'a' || str.charAt(j) >  'z') && (str.charAt(j) < '0' || str.charAt(j) >  '9')) {
                 j--;
                 if (i > j) {
                     return true;
