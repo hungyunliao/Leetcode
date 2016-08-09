2ed3b0cffc60283ae2f427971ce68fec700ff5c5
Hung-Yun Liao <hungyunliao@gmail.com>
2016-08-09 16:53:54 -0700

    Update 338-- Counting Bits.java

--- 338-- Counting Bits.java
+++ 338-- Counting Bits.java
@@ -1,12 +1,21 @@
 public class Solution {
     public int[] countBits(int num) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 0);
+        
+        int[] ans = new int[num + 1];
         int index = 0;
         int power = 1;
         
+        /*
+            pattern:
+            [0]
+            [0,  1] -> append previous array(0) + 1
+            [0,1,  1,2] -> append previous array(0,1) + 1
+            [0,1,1,2,  1,2,2,3] -> append previous array(0,1,1,2) + 1
+        */
+        
         for (int i = 1; i <= num; i++) {
-            map.put(i, map.get(index++) + 1);
+            
+            ans[i] = ans[index++] + 1;
             
             if (i == (power*2 - 1)) {
                 index = 0;
@@ -14,10 +23,6 @@ public class Solution {
             }
         }
         
-        int[] ans = new int[num+1];
-        for (int i = 0; i <= num; i++) {
-            ans[i] = map.get(i);
-        }
         return ans;
     }
 }
