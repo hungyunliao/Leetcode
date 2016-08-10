69a2b9c0ed8d96b94484b73162f0e071948f7e44
Hung-Yun Liao <hungyunliao@gmail.com>
2016-08-09 20:06:13 -0700

    Update 165-- Compare Version Numbers.java

--- 165-- Compare Version Numbers.java
+++ 165-- Compare Version Numbers.java
@@ -1,18 +1,22 @@
 public class Solution {
     public int compareVersion(String version1, String version2) {
-        int v1num = 0, v2num = 0;
+        
+        int diff = 0;
         Stack<Integer> stk = new Stack<>();
         int pow = 1;
         
         int i = 0, j = 0;
         
         while (i < version1.length() || j < version2.length()) {
+            
+            diff = 0;
+            
             while (i < version1.length() && version1.charAt(i) != '.') {
                 stk.push(version1.charAt(i) - '0');
                 i++;
             }
             while (stk.size() != 0) {
-                v1num += stk.pop() * pow;
+                diff += stk.pop() * pow;
                 pow *= 10;
             }
             pow = 1;
@@ -22,21 +26,22 @@ public class Solution {
                 j++;
             }
             while (stk.size() != 0) {
-                v2num += stk.pop() * pow;
+                diff -= stk.pop() * pow;
                 pow *= 10;
             }
             pow = 1;
             
-            if (v1num > v2num) {
+            if (diff > 0) {
                 return 1;
-            } else if (v1num < v2num) {
+            } else if (diff < 0) {
                 return -1;
             } else {
-                if (i < version1.length()) i++;
-                if (j < version2.length()) j++;
+                if (i < version1.length()) { i++; }
+                if (j < version2.length()) { j++; }
             }
             
         }
+        
         return 0;
         
     }
