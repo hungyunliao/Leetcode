14c3652c062ab4fe59057d1630876ae98490227a
Hung-Yun Liao <hungyunliao@gmail.com>
2016-10-21 17:03:10 -0700

    Update (M) 416- Partition Equal Subset Sum.java

--- (M) 416- Partition Equal Subset Sum.java
+++ (M) 416- Partition Equal Subset Sum.java
@@ -12,33 +12,39 @@ public class Solution {
         // dp[i][j] means given first 'i' element in the nums, can you use any of them to form the sum 'j'? If yes, True, False otherwise. (You can use some of the elements given, no need to use all of them)
         
         // Init first row. Given any sum, if you have an empty array, you cannot form anything.
-        for(int j = 0; j <= sum/2; j++) {
-            opt[j][0] = false;
+        for(int i = 0; i <= sum/2; i++) {
+            opt[i][0] = false;
         }
         
         // Init first col. Given any array, all can form the sum of '0' (Don't select anything.)
-        for(int i = 0; i <= nums.length; i++) {
-            opt[0][i] = true;
+        for(int j = 0; j <= nums.length; j++) {
+            opt[0][j] = true;
         }
         
         /*
+            The goal is to pick element in the array to another array Left, and the leftover elements will be the array Right,
+            so we want to see whether we can pick elements in the given array (nums[0] to nums[n]) to reach sum/2, which means the leftover elements will also from sum/2.
+            The subproblem is to exam whether the given array (0 to i) can form the given sum.
+            
+            
             The recurence will be:
-            opt(i, j) = opt(i - 1, j) || opt(i - 1, j - nums[0 to n - 1])
-            opt(i - 1, j): if given array length (i) cannot form the sum, the ans is simply like the previous one.
-            opt(i - 1, j - nums[0 to n]): if the given array length(i) can form the sum, meaning that you can find one j - nums[x] (x is from 0 to n-1) which forms the small subproblem with array size i - 1 and sum = j - element.
+            nums[last one available] : the last element in the given array
+            opt(i, j) = opt(i - 1, j) || opt(i - 1, j - nums[last one available])
+            opt(i - 1, j): if the last element is not picked, the sum is the same, the subproblem is like opt(i - 1, j)
+            opt(i - 1, j - nums[last one available]): if the last element is picked, the sum will be j - nums[last one available] for the subproblem.
         */
         
         for(int j = 1; j <= nums.length; j++) {
             for(int i = 1; i <= sum/2; i++) {
-                for(int x = (j - 1); x < j; x++) {
-                    if(nums[x] > sum/2) {
-                        return false;
-                    }
-                    if((i - nums[x]) >= 0) {
-                        opt[i][j] = opt[i][j - 1] || opt[i - nums[x]][j - 1];
-                    }
-                    else {
-                        opt[i][j] = opt[i][j - 1];
+                
+                int x = j - 1;
+                if(nums[x] > sum/2) {
+                    return false;
+                }
+                if((i - nums[x]) >= 0) {
+                    opt[i][j] = opt[i][j - 1] || opt[i - nums[x]][j - 1];
+                } else {
+                    opt[i][j] = opt[i][j - 1];
                 }
             }
         }
